daee41d0c004df9fc6d5ef56216410b73a89235b
Max <2202788@slu.edu.ph>
2022-07-21 19:01:13 +0800

    test

--- DATAMINING/settings.py
+++ DATAMINING/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-g#bwhnv!%=3a3p_)$jlj+g13+6d6s&n$b7(7%_s@+o!su#4dj-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'crime-mapping-finalproject.herokuapp.com'
+]
 
 
 # Application definition
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
